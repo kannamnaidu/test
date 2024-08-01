52b59ef33fedff993dbdaf774f9245f20b1ac8f0
kannamnaidu <116339196+kannamnaidu@users.noreply.github.com>
2024-08-01 12:54:48 +0530

    Update bq.py

--- bq.py
+++ bq.py
@@ -1,9 +1,11 @@
 from google.cloud import bigquery
 import sys
 config_parameters = {
-"QUERY_PROJECT_ID": "clarity-lm-idla-dev",
-"QUERY_BRANCH_NAME":"qa",
-"QUERY_BUCKET_NAME":"clarity-qa-bucket"
+"QUERY_PROJECT_ID": "clarity-lm-idla-stage",
+"QUERY_BRANCH_NAME":"uat",
+"QUERY_TOPIC_ID":"clarity_stage_topic",
+"QUERY_SUB_ID":"clarity_stage_sub",
+"QUERY_BUCKET_NAME":"clarity-stage-bucket"
 }
 file_name = sys.argv[1]
 
@@ -13,13 +15,14 @@ with open(file_name, 'r') as sql:
 for key,value in config_parameters.items():
     sql_query = sql_query.replace(key,value)
 
-sql_query =  sql_query.replace("dev_connector_etl_setting","qa_connector_etl_setting")
+sql_query =  sql_query.replace("dev_connector_etl_setting","uat_connector_etl_setting")
 
-client = bigquery.Client()
+client = bigquery.Client(project=config_parameters['QUERY_PROJECT_ID'])
 
 try:
     query_job = client.query(sql_query)
     query_job.result()
-except:
-    print (sql_query)
+except Exception as why:
+    # print (sql_query)
+    print(why)
     raise Exception(sys.exc_info())
